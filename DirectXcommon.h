87c6bcad52f708f94fd2c696080ce8358a6aa207
TsubokiHayato <146918548+TsubokiHayato@users.noreply.github.com>
2024-08-17 14:40:55 +0900

    Revert "a"
    This reverts commit a101be814b68874ae4252d0da591fd82cce85815.

--- DirectXcommon.h
+++ DirectXcommon.h
@@ -1,25 +1,11 @@
 #pragma once
-#include<d3d12.h>
-#include<dxgi1_6.h>
-#include<wrl.h>
-
 class DirectXcommon
 {
 public:
 	void Initialize();
 	void Update();
 
-
-	//デバイスの初期化
-	void Device_Initialize();
-	//コマンド関連の初期化
-	void Command_Initialize();
-	//スワップチェーンの生成
-	void SwapChain_Create();
 private:
-	//DirectXデバイス
-	Microsoft::WRL::ComPtr<ID3D12Device> device;
-	//DXGIファクトリ
-	Microsoft::WRL::ComPtr<IDXGIFactory7> dxgiFactory;
+
 };
 
