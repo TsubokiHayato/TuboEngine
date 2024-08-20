b8ec23eb21231fa8d8eab014dfe37fd482d9ca6e
TsubokiHayato <146918548+TsubokiHayato@users.noreply.github.com>
2024-08-20 13:10:38 +0900

    sa

--- main.cpp
+++ main.cpp
@@ -1,13 +1,6 @@
 
 #include<string>
 #include<format>
-#include<cassert>
-
-#include<d3d12.h>
-#include<dxgi1_6.h>
-
-#pragma comment(lib,"d3d12.lib")
-#pragma comment(lib,"dxgi.lib")
 
 #include<dxgidebug.h>
 #pragma comment(lib,"dxguid.lib")
@@ -30,7 +23,7 @@
 #include <fstream>
 #include<sstream>
 
-#include<wrl.h>
+
 
 
 #include "Input.h"
@@ -607,119 +600,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	DirectXcommon* dxCommon = nullptr;
 	dxCommon = new DirectXcommon();
 	dxCommon->Initialize();
-
-#ifdef _DEBUG
-
-	Microsoft::WRL::ComPtr <ID3D12Debug1> debugController = nullptr;
-	if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController)))) {
-		//デバックレイヤを有効化する
-		debugController->EnableDebugLayer();
-		//さらにGPU側でもチェックを行えるようにする
-		debugController->SetEnableGPUBasedValidation(true);
-
-	}
-
-#endif
-
-
-
-#pragma region DXGIFactory
-	/*DXGIFactoryの作成*/
-	//DXGIファクトリーの設置
-	Microsoft::WRL::ComPtr <IDXGIFactory7> dxgiFactory = nullptr;
-
-	//HRESULTはWindow系のエラーコードであり、
-	//関数が成功したかどうかSUCCEEDEDマクロで判断出来る
-	HRESULT hr = CreateDXGIFactory(IID_PPV_ARGS(&dxgiFactory));
-
-	//初期化の根本的な部分でエラーが出た場合はプログラムが間違っているか、
-	//どうにもできない場合が多いのでassertにしておく
-
-	assert(SUCCEEDED(hr));
-#pragma endregion
-
-#pragma region adapter
-	//使用するアダプタ用の変数
-	Microsoft::WRL::ComPtr <IDXGIAdapter4> useAdapter = nullptr;
-	//一番いい並びのアダプタを頼む
-	for (UINT i = 0; dxgiFactory->EnumAdapterByGpuPreference(i,
-		DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, IID_PPV_ARGS(&useAdapter)) !=
-		DXGI_ERROR_NOT_FOUND; ++i) {
-		//アダプタの情報取得
-		DXGI_ADAPTER_DESC3 adapterDesc{};
-		hr = useAdapter->GetDesc3(&adapterDesc);
-		assert(SUCCEEDED(hr));
-		//ソフトウェアアダプタ出ないならば採用
-		if (!(adapterDesc.Flags & DXGI_ADAPTER_FLAG3_SOFTWARE)) {
-			//採用したアダプタの情報をログに出力
-			Log(ConvertString(std::format(L"USE Adapter:{}\n", adapterDesc.Description)));
-			break;
-		}
-		//ソフトウェアアダプタならば見なかったことにする
-		useAdapter = nullptr;
-	}
-	//適切なアダプタが見つからなかったので起動できない
-	assert(useAdapter != nullptr);
-
-#pragma endregion
-
-#pragma region D3D12Device
-
-	Microsoft::WRL::ComPtr <ID3D12Device> device = nullptr;
-	//機能レベルとログ出力用の文字列
-	D3D_FEATURE_LEVEL featureLevels[] = {
-	D3D_FEATURE_LEVEL_12_2,D3D_FEATURE_LEVEL_12_1,D3D_FEATURE_LEVEL_12_0
-	};
-	const char* featureLevelStrings[] = { "12.2","12.1","12.0" };
-	//高い順採用できるか試していく
-	for (size_t i = 0; i < _countof(featureLevels); i++) {
-		//採用したアダプターでデバイスを生成
-		hr = D3D12CreateDevice(useAdapter.Get(), featureLevels[i], IID_PPV_ARGS(&device));
-		//指定した機能レベルでデバイスが生成できたか確認
-		if (SUCCEEDED(hr)) {
-			//生成できたのでロ出力をしてループを抜ける
-			Log(std::format("FeatureLevel : {}\n", featureLevelStrings[i]));
-			break;
-		}
-
-	}
-	//デバイスの生成がうまくいかなかったので起動できない
-	assert(device != nullptr);
-	//初期化完了のログを出す
-	Log("Complete create D3D12Device!!!\n");
-
-#pragma endregion 
-
-#ifdef _DEBUG
-	ID3D12InfoQueue* infoQueue = nullptr;
-	if (SUCCEEDED(device->QueryInterface(IID_PPV_ARGS(&infoQueue)))) {
-		//やばいエラー時に止まる
-		infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_CORRUPTION, true);
-		//エラー時に止まる
-		infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_ERROR, true);
-		//警告時に止まる
-		infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_WARNING, true);
-
-
-		D3D12_MESSAGE_ID denyIds[] = {
-			//window11でのDXGデバッグレイヤの相互作用バグによるエラーメッセージ
-			//https://stackoverflow.com/questions/69805245/directx-12-application-crashing-window-11
-			D3D12_MESSAGE_ID_RESOURCE_BARRIER_MISMATCHING_COMMAND_LIST_TYPE
-		};
-		//抑制するレベル
-		D3D12_MESSAGE_SEVERITY severities[] = { D3D12_MESSAGE_SEVERITY_INFO };
-		D3D12_INFO_QUEUE_FILTER filter{};
-		filter.DenyList.NumIDs = _countof(denyIds);
-		filter.DenyList.pIDList = denyIds;
-		filter.DenyList.NumSeverities = _countof(severities);
-		filter.DenyList.pSeverityList = severities;
-		//指定したメッセージの表示を抑制する
-		infoQueue->PushStorageFilter(&filter);
-		//解放
-		infoQueue->Release();
-
-	}
-#endif
 
 
 #pragma region commandQueue
