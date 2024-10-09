5aeeed2495fb09a80a6ba99c03e526785df98c6d
TsubokiHayato <146918548+TsubokiHayato@users.noreply.github.com>
2024-10-09 15:53:30 +0900

    kansei

--- Sprite.cpp
+++ Sprite.cpp
@@ -1,5 +1,146 @@
 #include "Sprite.h"
+#include"SpriteCommon.h"
+#include"MT_Matrix.h"
 
-void Sprite::Initialize()
+void Sprite::Initialize(SpriteCommon* spriteCommon, WinApp* winApp, DirectXCommon* dxCommon)
 {
+	this->spriteCommon = spriteCommon;
+	dxCommon_ = dxCommon;
+	winApp_ = winApp;
+	
+
+#pragma region SpriteResource
+
+	vertexResource = dxCommon->CreateBufferResource(sizeof(VertexData) * 6);
+
+	//頂点バッファビューを作成する
+	
+	//リソースの先頭のアドレスから使う
+	vertexBufferView.BufferLocation = vertexResource->GetGPUVirtualAddress();
+	//使用するリソースのサイズは頂点6つ分のサイズ
+	vertexBufferView.SizeInBytes = sizeof(VertexData) * 6;
+	//1頂点あたりのサイズ
+	vertexBufferView.StrideInBytes = sizeof(VertexData);
+
+	
+	vertexResource->Map(0, nullptr, reinterpret_cast<void**>(&vertexData));
+
+	//三角形を2つ使って、四角形の作る
+	/*---------------
+		B-------D		1枚目 : ABCの三角形
+		|		|		2枚目 : BDCの三角形
+		|		|
+		A-------C
+	----------------*/
+	//A
+	vertexData[0].position = { 0.0f,360.0f,0.0f,1.0f };
+	vertexData[0].texcoord = { 0.0f,1.0f };
+	vertexData[0].normal = { 0.0f,0.0f,1.0f };
+
+	//B
+
+	vertexData[1].position = { 0.0f,0.0f,0.0f,1.0f };
+	vertexData[1].texcoord = { 0.0f,0.0f };
+	vertexData[1].normal = { 0.0f,0.0f,1.0f };
+
+	//C
+
+	vertexData[2].position = { 640.0f,360.0f,0.0f,1.0f };
+	vertexData[2].texcoord = { 1.0f,1.0f };
+	vertexData[2].normal = { 0.0f,0.0f,1.0f };
+
+
+	//D
+	vertexData[3].position = { 640.0f,0.0f,0.0f,1.0f };
+	vertexData[3].texcoord = { 1.0f,0.0f };
+	vertexData[3].normal = { 0.0f,0.0f,1.0f };
+
+
+	transformationMatrixResource = dxCommon->CreateBufferResource(sizeof(TransformationMatrix));
+	//データを書き込む
+	transformationMatrixData = nullptr;
+
+	//書き込むためのアドレスを取得
+	transformationMatrixResource->Map(0, nullptr, reinterpret_cast<void**>(&transformationMatrixData));
+	//単位行列を書き込んでおく
+	transformationMatrixData->WVP = MakeIdentity4x4();
+	transformationMatrixData->World = MakeIdentity4x4();
+
+#pragma endregion
+
+
+#pragma region indexResourceSprite
+
+	//WVP用のリソースを作る
+	indexResource =dxCommon->CreateBufferResource(sizeof(uint32_t) * 6);
+
+
+	
+
+	indexBufferView.BufferLocation = indexResource->GetGPUVirtualAddress();
+
+	indexBufferView.SizeInBytes = sizeof(uint32_t) * 6;
+	indexBufferView.Format = DXGI_FORMAT_R32_UINT;
+
+	
+	indexResource->Map(0, nullptr, reinterpret_cast<void**>(&indexData));
+
+	indexData[0] = 0;    indexData[1] = 1;   indexData[2] = 2;
+	indexData[3] = 1;    indexData[4] = 3;   indexData[5] = 2;
+
+
+
+#pragma endregion
+
+
+
+#pragma region Material_Resource_Sprite
+	//マテリアル用のリソースを作る。今回はColor1つ分のサイズを用意する
+	materialResource =
+		dxCommon->CreateBufferResource(sizeof(Material));
+	//マテリアルにデータを書き込む
+	//書き込むためのアドレスを取得
+	materialResource->Map(0, nullptr, reinterpret_cast<void**>(&materialData));
+	//今回は白を書き込んでみる
+	materialData->color = { 1.0f, 1.0f, 1.0f, 1.0f };
+	materialData->enableLighting = false;
+	materialData->uvTransform = MakeIdentity4x4();
+
+#pragma endregion
+
+
+}
+
+void Sprite::Update()
+{
+	Matrix4x4 uvTransformMatrix = MakeAffineMatrix(uvTransFormMatrix.scale, uvTransFormMatrix.rotate, uvTransFormMatrix.translate);
+
+	materialData->uvTransform = uvTransformMatrix;
+
+	Matrix4x4 worldMatrix = MakeAffineMatrix(transformMatrix.scale, transformMatrix.rotate, transformMatrix.translate);
+	Matrix4x4 viewMatrix = MakeIdentity4x4();
+	Matrix4x4 projectionMatrix = MakeOrthographicMatrix(0.0f, 0.0f, float(winApp_->kClientWidth), float(winApp_->kClientHeight), 0.0f, 100.0f);
+	Matrix4x4 worldViewProjectionMatrix = Multiply(worldMatrix, Multiply(viewMatrix, projectionMatrix));
+	transformationMatrixData->WVP = worldViewProjectionMatrix;
+	transformationMatrixData->World = worldMatrix;
+
+	commandList = dxCommon_->GetCommandList();
+}
+
+void Sprite::Draw(D3D12_GPU_DESCRIPTOR_HANDLE textureSrvHandleGPU)
+{
+
+	//Spriteの描画
+	commandList->IASetVertexBuffers(0, 1, &vertexBufferView);
+	commandList->IASetIndexBuffer(&indexBufferView);
+
+	commandList->SetGraphicsRootConstantBufferView(0, materialResource->GetGPUVirtualAddress());
+
+	//TransformationMatrixCBufferの設定
+	commandList->SetGraphicsRootConstantBufferView(1, transformationMatrixResource->GetGPUVirtualAddress());
+
+	commandList->SetGraphicsRootDescriptorTable(2, textureSrvHandleGPU);
+	//描画
+	commandList->DrawIndexedInstanced(6, 1, 0, 0, 0);
+
 }
