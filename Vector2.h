768b401760d06970f92a999e072390843184e15f
TsubokiHayato <146918548+TsubokiHayato@users.noreply.github.com>
2024-10-15 16:18:39 +0900

    kansei

--- Vector2.h
+++ Vector2.h
@@ -5,4 +5,10 @@ struct Vector2
 	float x;
 	float y;
 
+    // += 演算子のオーバーロード
+    Vector2& operator+=(const Vector2& other) {
+        this->x += other.x;
+        this->y += other.y;
+        return *this;
+    }
 };
