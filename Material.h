984cac414803b01a20ca002995bdede0bd817738
TsubokiHayato <146918548+TsubokiHayato@users.noreply.github.com>
2024-11-18 17:30:59 +0900

    mikan

---
+++ Material.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <cstdint>
+#include "Vector4.h"
+#include "Matrix4x4.h"
+
+//マテリアル
+struct Material {
+	//	色
+	Vector4 color;
+	//ライティングを有効にするか
+	int32_t enableLighting;
+	//パディング
+	float padding[3];
+	//UV変換行列
+	Matrix4x4 uvTransform;
+};
