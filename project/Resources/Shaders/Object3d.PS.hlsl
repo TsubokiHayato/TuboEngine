b226d04000495ad547599cf4dbbb5f42bd34eac3
TsubokiHayato <146918548+TsubokiHayato@users.noreply.github.com>
2024-11-20 15:17:09 +0900

    FIle 管理終了???

---
+++ project/Resources/Shaders/Object3d.PS.hlsl
@@ -0,0 +1,68 @@
+#include"Object3d.hlsli"
+
+struct Material
+{
+    float4 color;
+    int enableLighting;
+    float4x4 uvTransform;
+};
+
+
+struct DirectionalLight
+{
+    float4 color;
+    float3 direction;
+    float intensity;
+};
+
+
+//コンスタントバッファの定義
+//使用例 : ConstantBuffer<構造体> 変数名 : register(b0);
+ConstantBuffer<Material> gMaterial : register(b0);
+Texture2D<float4> gTexture : register(t0);
+SamplerState gSampler : register(s0);
+ConstantBuffer<DirectionalLight> gDirectionalLight : register(b1);
+
+struct PixcelShaderOutput
+{
+    float4 color : SV_TARGET0;
+};
+
+
+PixcelShaderOutput main(VertexShaderOutPut input)
+{
+    PixcelShaderOutput output;
+  
+    
+    //float4 textureColor = gTexture.Sample(gSampler, input.texcoord);
+    float4 tranceformedUV = mul(float4(input.texcoord, 0.0f, 1.0f), gMaterial.uvTransform);
+    float4 textureColor = gTexture.Sample(gSampler, tranceformedUV.xy);
+   
+    
+    if (gMaterial.enableLighting != 0)
+    {
+        float NdotL = dot(normalize(input.normal), -gDirectionalLight.direction);
+        float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
+      
+      // output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
+       
+        
+        output.color.rgb = gMaterial.color.rgb * textureColor.rgb * gDirectionalLight.color.rgb * cos * gDirectionalLight.intensity;
+        output.color.a = gMaterial.color.a * textureColor.a;
+        
+    }
+    else
+    {
+        //output.color = gMaterial.color * textureColor;
+        output.color = gMaterial.color * textureColor;
+    }
+    
+    
+  
+    
+    return output;
+};
+
+
+
+
