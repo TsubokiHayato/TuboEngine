8bce99330c0b4df0f4ba42ccc75f21f9f5c247d9
TsubokiHayato <146918548+TsubokiHayato@users.noreply.github.com>
2025-01-22 10:51:58 +0900

    プロジェクトのリファクタリングと機能追加
    `CG2_01.vcxproj`ファイルから`ConvertString.cpp`のインクルードを削除しました。
    `CG2_01.vcxproj.filters`ファイルから`ConvertString.cpp`のフィルタを削除しました。
    `ConvertString.cpp`ファイルの全てのコードを削除しました。
    `DirectXCommon.cpp`で`this->winApp`の初期化を`std::make_unique<WinApp>(*winApp)`に変更しました。
    `DirectXCommon.h`に`#include "externals/DirectXTex/d3dx12.h"`を追加し、新しいメソッドを追加しました。
    `Order.cpp`で`Framework::Initialize()`、`Framework::Update()`、`Framework::Finalize()`の呼び出しを追加し、`Framework::ImguiPreDraw()`と`Framework::ImguiPostDraw()`の呼び出しを削除しました。
    `Order.h`にメソッドに関するコメントを追加しました。

--- project/engine/base/Framework/Order.cpp
+++ project/engine/base/Framework/Order.cpp
@@ -2,20 +2,23 @@
 
 void Order::Initialize()
 {
+	//初期化
 	Framework::Initialize();
 }
 
 void Order::Update()
 {
+	//更新
 	Framework::Update();
-
+	//ImGuiの受付開始
 	Framework::ImguiPreDraw();
-
+	//ImGuiの受付終了
 	Framework::ImguiPostDraw();
 }
 
 void Order::Finalize()
 {
+	//終了処理
 	Framework::Finalize();
 
 }
