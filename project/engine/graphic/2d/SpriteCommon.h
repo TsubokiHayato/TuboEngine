1258010d55806b27085a56b82e490e1413267c3d
TsubokiHayato <146918548+TsubokiHayato@users.noreply.github.com>
2025-01-28 17:03:52 +0900

    save

--- project/engine/graphic/2d/SpriteCommon.h
+++ project/engine/graphic/2d/SpriteCommon.h
@@ -3,6 +3,12 @@
 #include"DirectXcommon.h"
 #include<memory>
 class PSO;//前方宣言
+class NoneBlendPSO;
+class NormalBlendPSO;
+class AddBlendPSO;
+class MultiplyBlendPSO;
+class SubtractBlendPSO;
+class ScreenBlendPSO;
 class SpriteCommon
 {
 public:
@@ -16,19 +22,35 @@ public:
 	void Initialize(WinApp* winApp, DirectXCommon* dxCommon);
 
 	/// <summary>
-	/// 共通描画設定
-	/// </summary>
-	void DrawSettingsCommon();
+   /// 共通描画設定
+   /// </summary>
+   /// <param name="blendMode">ブレンドモード</param>
+   /// <remarks>
+   /// 0: NoneBlendPSO
+   /// 1: NormalBlendPSO
+   /// 2: AddBlendPSO
+   /// 3: SubtractBlendPSO
+   /// 4: MultiplyBlendPSO
+   /// 5: ScreenBlendPSO
+   /// </remarks>
+	void DrawSettingsCommon(int blendMode);
 	/*---------------------------------------------------
 			GETTER!
 	---------------------------------------------------*/
 	DirectXCommon* GetDxCommon()const { return dxCommon_; }
 	WinApp* GetWinApp()const { return winApp_; }
+
 private:
 	WinApp* winApp_ = nullptr;
 	DirectXCommon* dxCommon_;
 	PSO* pso = nullptr;
+	NoneBlendPSO* noneBlendPSO = nullptr;//NoneBlendPSO
+	NormalBlendPSO* normalBlendPSO = nullptr;//NormalBlendPSO
+	AddBlendPSO* addBlendPSO = nullptr;//AddBlendPSO
+	MultiplyBlendPSO* multiplyBlendPSO = nullptr;//MultiplyBlendPSO
+	SubtractBlendPSO* subtractBlendPSO = nullptr;//SubtractBlendPSO
+	ScreenBlendPSO* screenBlendPSO = nullptr;//ScreenBlendPSO
 
-
+	int blenderMode_;//ブレンダーモード
 };
 
