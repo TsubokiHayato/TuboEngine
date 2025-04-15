c5400a0630660118fc27dbb757128c5ed9a336b7
TsubokiHayato <k023g0088@g.neec.ac.jp>
2025-04-15 11:14:33 +0900

    パーティクルシステムの型変更と機能追加
    `Particle` クラスの `velocity` パラメータの型を `Vector4` から `Vector3` に変更しました。また、`CreateNewParticle` メソッドでパーティクル生成時に `velocity`、`color`、`lifeTime`、`currentTime` を設定するように修正しました。`ParticleEmitter` クラスのコンストラクタと `velocity_` メンバ変数の型を `Vector2` から `Vector3` に変更し、Getter と Setter メソッドを追加しました。`TitleScene` クラスの `particleVelocity` の型を `Vector4` から `Vector3` に変更し、乱数エンジンと `RandomRange` 構造体を追加しました。`imgui.ini` ファイルのウィンドウ位置を微調整しました。

--- project/engine/graphic/Particle/ParticleEmitter.cpp
+++ project/engine/graphic/Particle/ParticleEmitter.cpp
@@ -8,39 +8,58 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 
-
-ParticleEmitter::ParticleEmitter(Particle* particle, const std::string& name, const Transform& transform, uint32_t count, float frequency, bool repeat)
-  //パーティクルのインスタンス
-    : particle_(particle), 
-    //パーティクルグループ名
-    name_(name), 
-    // エミッターの位置・回転・スケール
-    transform_(transform), 
-    // 発生させるパーティクルの数
-    count_(count), 
-    // 発生頻度
-    frequency_(frequency),
-    // 発生頻度
-    elapsedTime_(frequency),
-    // 繰り返し発生させるかどうかのフラグ
-    repeat_(repeat)
-{
-    Emit(); // 初期化時に即時発生
+// コンストラクタ
+	/// @brief パーティクルエミッターのコンストラクタ
+	/// @param particle パーティクルのインスタンス
+	/// @param name パーティクルグループ名
+	/// @param transform エミッターの位置・回転・スケール
+	/// @param velocity 速度
+	/// @param color カラー
+	/// @param lifeTime 寿命
+	/// @param currentTime 経過時間
+	/// @param count 発生させるパーティクルの数
+	/// @param frequency 発生頻度
+	/// @param repeat 繰り返し発生させるかどうかのフラグ
+ParticleEmitter::ParticleEmitter(Particle* particle, const std::string& name,
+	const Transform& transform, Vector3 velocity, Vector4 color, float lifeTime, float currentTime,
+	uint32_t count, float frequency, bool repeat)
+	//パーティクルのインスタンス
+	: particle_(particle),
+	//パーティクルグループ名
+	name_(name),
+	// エミッターの位置・回転・スケール
+	transform_(transform),
+	// 速度
+	velocity_(velocity),
+	// カラー
+	color_(color),
+	// 寿命
+	lifeTime_(lifeTime),
+	// 経過時間
+	currentTime_(currentTime),
+	// 発生させるパーティクルの数
+	count_(count),
+	// 発生頻度
+	frequency_(frequency),
+	// 発生頻度
+	elapsedTime_(frequency),
+	// 繰り返し発生させるかどうかのフラグ
+	repeat_(repeat) {
+	Emit(); // 初期化時に即時発生
 }
 
 void ParticleEmitter::Update() {
 	if (!repeat_) return; // 繰り返し発生させない場合は何もしない
 
-    elapsedTime_ += 1.0f / 60.0f; // フレーム単位の経過時間を加算
+	elapsedTime_ += 1.0f / 60.0f; // フレーム単位の経過時間を加算
 
 	// 経過時間が発生頻度を超えたらパーティクルを発生させる
-    if (elapsedTime_ >= frequency_)
-    {
+	if (elapsedTime_ >= frequency_) {
 		// パーティクルの発生
-        Emit();
-        // 周期的に実行するためリセット
-        elapsedTime_ -= frequency_;
-    }
+		Emit();
+		// 周期的に実行するためリセット
+		elapsedTime_ -= frequency_;
+	}
 }
 
 void ParticleEmitter::Draw() {
@@ -54,5 +73,5 @@ void ParticleEmitter::Emit() {
 
 void ParticleEmitter::SetRepeat(bool repeat) {
 	// 繰り返し発生させるかどうかのフラグを設定
-    repeat_ = repeat;
+	repeat_ = repeat;
 }
