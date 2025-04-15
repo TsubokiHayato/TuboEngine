c5099363b356d9c5789e13c4d32a467634e36c77
TsubokiHayato <k023g0088@g.neec.ac.jp>
2025-04-15 10:43:59 +0900

    パーティクルシステムの引数とメンバ変数を追加 Particle::Emit 関数と Particle::CreateNewParticle 関数に Vector4 velocity, Vector4 color, float lifeTime, float currentTime の引数を追加しました。ParticleEmitter クラスのコンストラクタ、 メンバ変数、および Emit 関数に同様の引数を追加しました。 TitleScene::Initialize 関数で ParticleEmitter のインスタンスを 作成する際に新しい引数を追加し、TitleScene クラスに対応する メンバ変数を追加しました。

--- project/engine/graphic/Particle/ParticleEmitter.h
+++ project/engine/graphic/Particle/ParticleEmitter.h
@@ -8,13 +8,20 @@ class ParticleEmitter {
 public:
 
     // コンストラクタ
+	/// @brief パーティクルエミッターのコンストラクタ
 	/// @param particle パーティクルのインスタンス
 	/// @param name パーティクルグループ名
 	/// @param transform エミッターの位置・回転・スケール
+	/// @param velocity 速度
+	/// @param color カラー
+	/// @param lifeTime 寿命
+	/// @param currentTime 経過時間
 	/// @param count 発生させるパーティクルの数
 	/// @param frequency 発生頻度
 	/// @param repeat 繰り返し発生させるかどうかのフラグ
-    ParticleEmitter(Particle* particle, const std::string& name, const Transform& transform, uint32_t count, float frequency, bool repeat = false);
+	ParticleEmitter(Particle* particle, const std::string& name, 
+		const Transform& transform, Vector2 velocity, Vector4 color, float lifeTime, float currentTime,
+		uint32_t count, float frequency, bool repeat = false);
 
 
 	//更新
@@ -33,6 +40,10 @@ private:
     Particle* particle_; // Particleのインスタンスを保持
     std::string name_;   // パーティクルグループ名
     Transform transform_;// エミッターの位置・回転・スケール
+	Vector4 velocity_; // 速度
+	Vector4 color_;    // カラー
+	float lifeTime_;   // 寿命
+	float currentTime_; // 経過時間
     uint32_t count_;     // 発生させるパーティクルの数
     float frequency_;    // 発生頻度
     float elapsedTime_;  // 経過時間
