520ac4cb86104dcc3fd8f4d190f74e426bcbd138
TsubokiHayato <k023g0088@g.neec.ac.jp>
2025-05-08 17:04:37 +0900

    レンダーターゲット管理と描画処理の分離を実装 DirectX 12 を使用したレンダーターゲット管理を改善し、描画処理を分離しました。`OffScreenRendering` クラスの整理に伴い、`DirectXCommon` クラスにレンダーターゲットの作成・クリア処理を移動しました。また、`Framework` クラスではレンダーターゲット描画とスワップチェーン描画を分離し、描画フローを明確化しました。これにより、コードの可読性とメンテナンス性が向上し、レンダーターゲットを活用した描画処理の拡張が容易になりました。

--- project/engine/base/Framework/Order.cpp
+++ project/engine/base/Framework/Order.cpp
@@ -23,18 +23,26 @@ void Order::Finalize()
 
 void Order::Draw()
 {
-	//ループ前処理
-	Framework::FrameworkPreDraw();
+
+	//RenderTargetの描画
+	
+	Framework::FrameWorkRenderTargetPreDraw();
 	//3Dオブジェクト描画
 	Framework::Object3dCommonDraw();
 	//2Dスプライト描画
 	Framework::SpriteCommonDraw();
 	//パーティクル描画
 	Framework::ParticleCommonDraw();
+	Framework::FrameWorkRenderTargetPostDraw();
+
+	//SwapChainの描画
+
+	//ループ前処理
+	Framework::FrameworkSwapChainPreDraw();
 	//ImGuiの受付開始
 	Framework::ImguiPreDraw();
 	//ImGuiの受付終了
 	Framework::ImguiPostDraw();
 	//ループ後処理
-	Framework::FrameworkPostDraw();
+	Framework::FrameworkSwapChainPostDraw();
 }
