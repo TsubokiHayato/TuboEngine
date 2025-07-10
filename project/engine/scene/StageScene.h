ccd7dd4c5c982cda9ee8eb6121a0092bdc3ad500
tubosan <146918548+TsubokiHayato@users.noreply.github.com>
2025-07-10 10:10:56 +0900

    Revert "各基盤シングルトン化適用"

--- project/engine/scene/StageScene.h
+++ project/engine/scene/StageScene.h
@@ -1,27 +1,8 @@
 #pragma once
 
-#include "Input.h"
-#include"Audio.h"
-#include"Camera.h"
-#include"CollisionManager.h"
-#include"application/FollowTopDownCamera.h"
-
-#include"SpriteCommon.h"
-#include"Object3dCommon.h"
-#include"ModelCommon.h"
-#include"TextureManager.h"
-#include"ModelManager.h"
-#include"AudioCommon.h"
-#include"ImGuiManager.h"
-#include "Collider.h"
-
-#include"application/Player.h"
-#include"application/PlayerBullet.h"
-#include"application/Enemy.h"
-
-
-
-class StageScene : public IScene {
+#include"IScene.h"
+class StageScene :public IScene
+{
 public:
 	/// <summary>
 	/// 初期化
@@ -57,45 +38,6 @@ public:
 	/// <summary>
 	/// パーティクル描画
 	/// </summary>
-	void ParticleDraw() override;
-
-
-	void CheckAllCollisions();
-	
-private:
-
-	///----------------------------------------------------------------------------------------
-	///				引き渡し用変数
-	///-----------------------------------------------------------------------------------------
-
-	WinApp* winApp = nullptr;
-	DirectXCommon* dxCommon = nullptr;
-	Object3dCommon* object3dCommon = nullptr;
-	SpriteCommon* spriteCommon = nullptr;
-
-private:
-	///----------------------------------------------------------------------------------------
-	///				メンバ変数
-	///----------------------------------------------------------------------------------------
-
-	/// Audio///
-	
-	std::unique_ptr<Audio> audio = nullptr;
-
-	/// Collider ///
-	std::unique_ptr<CollisionManager> collisionManager_;
-
-	/// Camera ///
-	std::unique_ptr<FollowTopDownCamera> followCamera;
-    
-	Vector3 cameraPosition = {0.0f, 0.0f, -5.0f};
-	Vector3 cameraRotation = {0.0f, 0.0f, 0.0f};
-	Vector3 cameraScale = {1.0f, 1.0f, 1.0f};
-	
-	/// Player ///
-	std::unique_ptr<Player> player_ = nullptr;
-	/// Enemy ///
-	std::unique_ptr<Enemy> enemy_ = nullptr;
-	std::vector<std::unique_ptr<Enemy>> enemies; // Enemyリスト
-
+	void ParticleDraw()override;
+};
 
