07c3b4f57df4d6f7f67eefd3bbbbbd651b2c1bc2
TsubokiHayato <k023g0088@g.neec.ac.jp>
2026-02-16 15:29:04 +0900

    PlayerBullet系にImGui用グローバルUI関数を追加
    PlayerBulletおよびShotgunPelletBullet, PiercingBulletにImGui用の静的関数DrawImGuiGlobal()を追加し、将来的なパラメータ編集UI拡張の準備を行いました。また、PlayerBulletに3Dオブジェクトのメンバ変数や、マップ・プレイヤー情報取得用の関数を追加し、派生弾での利用性を向上。Player.cppではImGui描画時に各弾種のグローバルUIも呼び出すように変更。imgui.iniにはBounceBullet (Global)ウィンドウの情報を追加しました。

--- project/application/Bullet/Player/PiercingBullet.h
+++ project/application/Bullet/Player/PiercingBullet.h
@@ -9,4 +9,8 @@ class PiercingBullet final : public PlayerBullet {
 public:
 	bool ShouldDieOnEnemyHit() const override { return false; }
 	void OnHitEnemy(Collider* other) override { (void)other; }
+
+	static void DrawImGuiGlobal() {
+		// 現状は固有パラメータ無し。将来のダメージ倍率等を追加する場所。
+	}
 };
