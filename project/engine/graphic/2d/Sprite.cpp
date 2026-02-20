11658374002130c0612f11327cd50460e5205cce
TsubokiHayato <k023g0088@g.neec.ac.jp>
2026-02-20 11:11:37 +0900

    メンバ変数名に一貫性を持たせるリファクタリング
    `Sprite` クラスのメンバ変数名にアンダースコア (`_`) を追加し、命名規則を統一しました。これにより、メンバ変数とローカル変数の区別が明確になり、コードの可読性と保守性が向上しました。
    
    主な変更点:
    - `Sprite::Initialize`、`Sprite::Update`、`Sprite::Draw` メソッド内の変数名を変更。
    - `Sprite.h` 内のメンバ変数とメソッドの命名規則を統一。
    - 頂点データやインデックスデータのポインタ変数名を変更。
    - テクスチャ番号やコマンドリストの変数名を変更。
    
    これらの変更により、コードの一貫性を確保し、将来的な拡張やデバッグが容易になります。

--- project/engine/graphic/2d/Sprite.cpp
+++ project/engine/graphic/2d/Sprite.cpp
@@ -16,19 +16,19 @@ void Sprite::Initialize(std::string textureFilePath)
 
 #pragma region SpriteResource
 
-	vertexResource = TuboEngine::DirectXCommon::GetInstance()->CreateBufferResource(sizeof(VertexData) * 6);
+	vertexResource_ = TuboEngine::DirectXCommon::GetInstance()->CreateBufferResource(sizeof(VertexData) * 6);
 
 	//頂点バッファビューを作成する
 
 	//リソースの先頭のアドレスから使う
-	vertexBufferView.BufferLocation = vertexResource->GetGPUVirtualAddress();
+	vertexBufferView_.BufferLocation = vertexResource_->GetGPUVirtualAddress();
 	//使用するリソースのサイズは頂点6つ分のサイズ
-	vertexBufferView.SizeInBytes = sizeof(VertexData) * 6;
+	vertexBufferView_.SizeInBytes = sizeof(VertexData) * 6;
 	//1頂点あたりのサイズ
-	vertexBufferView.StrideInBytes = sizeof(VertexData);
+	vertexBufferView_.StrideInBytes = sizeof(VertexData);
 
 
-	vertexResource->Map(0, nullptr, reinterpret_cast<void**>(&vertexData));
+	vertexResource_->Map(0, nullptr, reinterpret_cast<void**>(&vertexData_));
 
 	//三角形を2つ使って、四角形の作る
 	/*---------------
@@ -38,38 +38,38 @@ void Sprite::Initialize(std::string textureFilePath)
 		A-------C
 	----------------*/
 	//A
-	vertexData[0].position = { 0.0f,360.0f,0.0f,1.0f };
-	vertexData[0].texcoord = { 0.0f,1.0f };
-	vertexData[0].normal = { 0.0f,0.0f,1.0f };
+	vertexData_[0].position = { 0.0f,360.0f,0.0f,1.0f };
+	vertexData_[0].texcoord = { 0.0f,1.0f };
+	vertexData_[0].normal = { 0.0f,0.0f,1.0f };
 
 	//B
 
-	vertexData[1].position = { 0.0f,0.0f,0.0f,1.0f };
-	vertexData[1].texcoord = { 0.0f,0.0f };
-	vertexData[1].normal = { 0.0f,0.0f,1.0f };
+	vertexData_[1].position = { 0.0f,0.0f,0.0f,1.0f };
+	vertexData_[1].texcoord = { 0.0f,0.0f };
+	vertexData_[1].normal = { 0.0f,0.0f,1.0f };
 
 	//C
 
-	vertexData[2].position = { 640.0f,360.0f,0.0f,1.0f };
-	vertexData[2].texcoord = { 1.0f,1.0f };
-	vertexData[2].normal = { 0.0f,0.0f,1.0f };
+	vertexData_[2].position = { 640.0f,360.0f,0.0f,1.0f };
+	vertexData_[2].texcoord = { 1.0f,1.0f };
+	vertexData_[2].normal = { 0.0f,0.0f,1.0f };
 
 
 	//D
-	vertexData[3].position = { 640.0f,0.0f,0.0f,1.0f };
-	vertexData[3].texcoord = { 1.0f,0.0f };
-	vertexData[3].normal = { 0.0f,0.0f,1.0f };
+	vertexData_[3].position = { 640.0f,0.0f,0.0f,1.0f };
+	vertexData_[3].texcoord = { 1.0f,0.0f };
+	vertexData_[3].normal = { 0.0f,0.0f,1.0f };
 
 
-	transformationMatrixResource = TuboEngine::DirectXCommon::GetInstance()->CreateBufferResource(sizeof(TransformationMatrix));
+	transformationMatrixResource_ = TuboEngine::DirectXCommon::GetInstance()->CreateBufferResource(sizeof(TransformationMatrix));
 	//データを書き込む
-	transformationMatrixData = nullptr;
+	transformationMatrixData_ = nullptr;
 
 	//書き込むためのアドレスを取得
-	transformationMatrixResource->Map(0, nullptr, reinterpret_cast<void**>(&transformationMatrixData));
+	transformationMatrixResource_->Map(0, nullptr, reinterpret_cast<void**>(&transformationMatrixData_));
 	//単位行列を書き込んでおく
-	transformationMatrixData->WVP = MakeIdentity4x4();
-	transformationMatrixData->World = MakeIdentity4x4();
+	transformationMatrixData_->WVP = MakeIdentity4x4();
+	transformationMatrixData_->World = MakeIdentity4x4();
 
 
 #pragma endregion
@@ -78,17 +78,17 @@ void Sprite::Initialize(std::string textureFilePath)
 #pragma region indexResourceSprite
 
 	//WVP用のリソースを作る
-	indexResource = TuboEngine::DirectXCommon::GetInstance()->CreateBufferResource(sizeof(uint32_t) * 6);
-	indexBufferView.BufferLocation = indexResource->GetGPUVirtualAddress();
+	indexResource_ = TuboEngine::DirectXCommon::GetInstance()->CreateBufferResource(sizeof(uint32_t) * 6);
+	indexBufferView_.BufferLocation = indexResource_->GetGPUVirtualAddress();
 
-	indexBufferView.SizeInBytes = sizeof(uint32_t) * 6;
-	indexBufferView.Format = DXGI_FORMAT_R32_UINT;
+	indexBufferView_.SizeInBytes = sizeof(uint32_t) * 6;
+	indexBufferView_.Format = DXGI_FORMAT_R32_UINT;
 
 
-	indexResource->Map(0, nullptr, reinterpret_cast<void**>(&indexData));
+	indexResource_->Map(0, nullptr, reinterpret_cast<void**>(&indexData_));
 
-	indexData[0] = 0;    indexData[1] = 1;   indexData[2] = 2;
-	indexData[3] = 1;    indexData[4] = 3;   indexData[5] = 2;
+	indexData_[0] = 0;    indexData_[1] = 1;   indexData_[2] = 2;
+	indexData_[3] = 1;    indexData_[4] = 3;   indexData_[5] = 2;
 
 
 
@@ -98,20 +98,20 @@ void Sprite::Initialize(std::string textureFilePath)
 
 #pragma region Material_Resource_Sprite
 	//マテリアル用のリソースを作る。今回はColor1つ分のサイズを用意する
-	materialResource = TuboEngine::DirectXCommon::GetInstance()->CreateBufferResource(sizeof(Material));
+	materialResource_ = TuboEngine::DirectXCommon::GetInstance()->CreateBufferResource(sizeof(Material));
 	//マテリアルにデータを書き込む
 	//書き込むためのアドレスを取得
-	materialResource->Map(0, nullptr, reinterpret_cast<void**>(&materialData));
+	materialResource_->Map(0, nullptr, reinterpret_cast<void**>(&materialData_));
 	//今回は白を書き込んでみる
-	materialData->color = { 1.0f, 1.0f, 1.0f, 1.0f };
-	materialData->enableLighting = false;
-	materialData->uvTransform = MakeIdentity4x4();
+	materialData_->color = { 1.0f, 1.0f, 1.0f, 1.0f };
+	materialData_->enableLighting = false;
+	materialData_->uvTransform = MakeIdentity4x4();
 
 
 #pragma endregion
 
 
-	textureIndex = TextureManager::GetInstance()->GetSrvIndex(textureFilePath);
+	textureIndex_ = TextureManager::GetInstance()->GetSrvIndex(textureFilePath);
 
 	AdjustTextureSize();
 }
@@ -165,46 +165,46 @@ void Sprite::Update()
 	テクスチャの位置、画像位置, 法線ベクトル, 大きさ
 	---------------------------------------*/
 
-	transform.translate = { position_.x,position_.y,0.0f };
-	transform.rotate = { 0.0f,0.0f,rotation_ };
+	transform_.translate = { position_.x,position_.y,0.0f };
+	transform_.rotate = { 0.0f,0.0f,rotation_ };
 
-	vertexData[0].position = { left,bottom,0.0f,1.0f };
-	vertexData[0].texcoord = { tex_left,tex_bottom };
-	vertexData[0].normal = { 0.0f,0.0f,-1.0f };
+	vertexData_[0].position = { left,bottom,0.0f,1.0f };
+	vertexData_[0].texcoord = { tex_left,tex_bottom };
+	vertexData_[0].normal = { 0.0f,0.0f,-1.0f };
 
-	vertexData[1].position = { left,top,0.0f,1.0f };
-	vertexData[1].texcoord = { tex_left,tex_top };
-	vertexData[1].normal = { 0.0f,0.0f,-1.0f };
+	vertexData_[1].position = { left,top,0.0f,1.0f };
+	vertexData_[1].texcoord = { tex_left,tex_top };
+	vertexData_[1].normal = { 0.0f,0.0f,-1.0f };
 
-	vertexData[2].position = { right,bottom,0.0f,1.0f };
-	vertexData[2].texcoord = { tex_right,tex_bottom };
-	vertexData[2].normal = { 0.0f,0.0f,-1.0f };
+	vertexData_[2].position = { right,bottom,0.0f,1.0f };
+	vertexData_[2].texcoord = { tex_right,tex_bottom };
+	vertexData_[2].normal = { 0.0f,0.0f,-1.0f };
 
-	vertexData[3].position = { right,top,0.0f,1.0f };
-	vertexData[3].texcoord = { tex_right,tex_top };
-	vertexData[3].normal = { 0.0f,0.0f,-1.0f };
+	vertexData_[3].position = { right,top,0.0f,1.0f };
+	vertexData_[3].texcoord = { tex_right,tex_top };
+	vertexData_[3].normal = { 0.0f,0.0f,-1.0f };
 
-	transform.scale = { size_.x,size_.y,1.0f };
+	transform_.scale = { size_.x,size_.y,1.0f };
 
 
 	/*---------
 	行列更新処理
 	---------*/
-	Matrix4x4 uvTransformMatrix = MakeAffineMatrix(uvTransFormMatrix.scale, uvTransFormMatrix.rotate, uvTransFormMatrix.translate);
+	Matrix4x4 uvTransformMatrix = MakeAffineMatrix(uvTransFormMatrix_.scale, uvTransFormMatrix_.rotate, uvTransFormMatrix_.translate);
 
-	materialData->uvTransform = uvTransformMatrix;
+	materialData_->uvTransform = uvTransformMatrix;
 
-	Matrix4x4 worldMatrix = MakeAffineMatrix(transform.scale, transform.rotate, transform.translate);
+	Matrix4x4 worldMatrix = MakeAffineMatrix(transform_.scale, transform_.rotate, transform_.translate);
 	Matrix4x4 viewMatrix = MakeIdentity4x4();
 	Matrix4x4 projectionMatrix =
 	    MakeOrthographicMatrix(0.0f, 0.0f, float(TuboEngine::WinApp::GetInstance()->GetClientWidth()), float(TuboEngine::WinApp::GetInstance()->GetClientHeight()), 0.0f, 100.0f);
 	Matrix4x4 worldViewProjectionMatrix = Multiply(worldMatrix, Multiply(viewMatrix, projectionMatrix));
-	transformationMatrixData->WVP = worldViewProjectionMatrix;
-	transformationMatrixData->World = worldMatrix;
+	transformationMatrixData_->WVP = worldViewProjectionMatrix;
+	transformationMatrixData_->World = worldMatrix;
 
 
 
-	commandList = TuboEngine::DirectXCommon::GetInstance()->GetCommandList();
+	commandList_ = TuboEngine::DirectXCommon::GetInstance()->GetCommandList();
 
 
 
@@ -215,17 +215,17 @@ void Sprite::Draw(){
 
 	
 
-	commandList->IASetVertexBuffers(0, 1, &vertexBufferView);
-	commandList->IASetIndexBuffer(&indexBufferView);
+	commandList_->IASetVertexBuffers(0, 1, &vertexBufferView_);
+	commandList_->IASetIndexBuffer(&indexBufferView_);
 
-	commandList->SetGraphicsRootConstantBufferView(0, materialResource->GetGPUVirtualAddress());
+	commandList_->SetGraphicsRootConstantBufferView(0, materialResource_->GetGPUVirtualAddress());
 
 	//TransformationMatrixCBufferの設定
-	commandList->SetGraphicsRootConstantBufferView(1, transformationMatrixResource->GetGPUVirtualAddress());
+	commandList_->SetGraphicsRootConstantBufferView(1, transformationMatrixResource_->GetGPUVirtualAddress());
 
-	commandList->SetGraphicsRootDescriptorTable(2, TextureManager::GetInstance()->GetSrvHandleGPU(textureFilePath_));
+	commandList_->SetGraphicsRootDescriptorTable(2, TextureManager::GetInstance()->GetSrvHandleGPU(textureFilePath_));
 	//描画
-	commandList->DrawIndexedInstanced(6, 1, 0, 0, 0);
+	commandList_->DrawIndexedInstanced(6, 1, 0, 0, 0);
 
 }
 
