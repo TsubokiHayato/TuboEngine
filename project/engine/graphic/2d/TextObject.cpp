9cbe60217484e05406c66615c765bf3c9ae8ce99
TsubokiHayato <k023g0088@g.neec.ac.jp>
2026-05-22 13:56:25 +0900

    プレイヤー死亡時の弾消去・回避移動修正・UI調整
    プレイヤー死亡時に弾を全消去するようにし、回避移動時に壁衝突時の慣性暴走を防止しました。ステージ再開時のメッセージ表示処理を削除し、セーブメッセージのフェードアウトを線形減衰に修正。TextObjectの色変更時にマテリアルも更新するよう対応しました。

--- project/engine/graphic/2d/TextObject.cpp
+++ project/engine/graphic/2d/TextObject.cpp
@@ -94,6 +94,9 @@ void TextObject::SetPosition(const Math::Vector2& pos) {
 void TextObject::SetColor(const Math::Vector4& color) {
     if (color_.x != color.x || color_.y != color.y || color_.z != color.z || color_.w != color.w) {
         color_ = color;
+        if (materialData_) {
+            materialData_->color = color_;
+        }
         dirty_ = true;
     }
 }
